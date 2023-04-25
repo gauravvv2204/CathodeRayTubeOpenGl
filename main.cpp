146da646957b59512acb5295ca5395cb4d639c88
Gaurav Pahwa <81811468+gauravvv2204@users.noreply.github.com>
2023-04-25 16:24:00 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,596 @@
+#include <GL/glut.h>
+#include <unistd.h>
+#include <bits/stdc++.h>
+using namespace std;
+
+int width = 1100;
+int height = 500;
+struct Color {
+	GLfloat r;
+	GLfloat g;
+	GLfloat b;
+};
+
+float br = 0.81;
+float bg = 0.71;
+float bb = 0.65;
+
+
+void drawEllipse(int a, int b, int xc, int yc) {
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    float x = 0, y = b;
+    float d1 = (b * b) - (a * a * b) + (0.25 * a * a);
+    float dx = 2 * b * b * x;
+    float dy = 2 * a * a * y;
+
+    while (dx < dy) {
+        glBegin(GL_POINTS);
+        glVertex2f(x + xc, y + yc);
+        glVertex2f(-x + xc, y + yc);
+        glVertex2f(x + xc, -y + yc);
+        glVertex2f(-x + xc, -y + yc);
+        glEnd();
+        glFlush();
+        usleep(10000);
+        x++;
+        dx = dx + (2 * b * b);
+        if (d1 < 0) {
+            d1 = d1 + dx + (b * b);
+        } else {
+            y--;
+            dy = dy - (2 * a * a);
+            d1 = d1 + dx - dy + (b * b);
+        }
+    }
+
+    float d2 = ((b * b) * ((x + 0.5) * (x + 0.5))) +
+               ((a * a) * ((y - 1) * (y - 1))) -
+               (a * a * b * b);
+
+    while (y >= 0) {
+        glBegin(GL_POINTS);
+        glVertex2f(x + xc, y + yc);
+        glVertex2f(-x + xc, y + yc);
+        glVertex2f(x + xc, -y + yc);
+        glVertex2f(-x + xc, -y + yc);
+        glEnd();
+        glFlush();
+        usleep(10000);
+        y--;
+        dy = dy - (2 * a * a);
+        if (d2 > 0) {
+            d2 = d2 - dy + (a * a);
+        } else {
+            x++;
+            dx = dx + (2 * b * b);
+            d2 = d2 + dx - dy + (a * a);
+        }
+    }
+}
+
+void bresenham(int x1,int y1,int x2,int y2) {
+	int dx = x2 - x1;
+	int dy = y2 - y1;
+	int xinc=1,yinc=1;
+	if(dx<0) xinc = -1;
+	if(dy<0) yinc = -1;
+	dx = abs(dx);
+	dy = abs(dy);
+	int dy2 = 2 * dy;
+	int dydx2 = 2 * dy - 2 * dx;
+	int dx2 = 2 * dx;
+	int dxdy2 = 2 * dx - 2 * dy;
+	int x = x1;
+	int y = y1;
+	int p;
+	if (dx>dy) {
+		p = 2 * dy - dx;
+		glBegin(GL_POINTS);
+		glVertex2i(x, y);
+		for (int i = 0; i < dx; i++) {
+			if (p < 0) {
+				p = p+dy2;
+				x = x + xinc;
+				glBegin(GL_POINTS);
+				glVertex2i(x, y);
+				glEnd();
+				glFlush();
+				usleep(100);
+			}
+			else {
+				p = p + dydx2;
+				x = x + xinc;
+				y = y + yinc;
+				glBegin(GL_POINTS);
+				glVertex2i(x, y);
+				glEnd();
+				glFlush();
+				usleep(100);
+			}
+		}
+    }
+	else {
+		p = 2 * dx - dy;
+		glBegin(GL_POINTS);
+		glVertex2i(x, y);
+		for (int i = 0; i < dy; i++) {
+			if (p < 0) {
+				p = p + dx2;
+				y = y + yinc;
+				glBegin(GL_POINTS);
+				glVertex2i(x, y);
+				glEnd();
+				glFlush();
+				usleep(1000);
+			}
+			else {
+				p = p + dxdy2;
+				x = x + xinc;
+				y = y + yinc;
+				glBegin(GL_POINTS);
+				glVertex2i(x, y);
+				glEnd();
+				glFlush();
+				usleep(1000);
+			}
+		}
+		glEnd();
+	}
+}
+
+void bresenhamWithouDelay(int x1,int y1,int x2,int y2) {
+	int dx = x2 - x1;
+	int dy = y2 - y1;
+	int xinc=1,yinc=1;
+	if(dx<0) xinc = -1;
+	if(dy<0) yinc = -1;
+	dx = abs(dx);
+	dy = abs(dy);
+	int dy2 = 2 * dy;
+	int dydx2 = 2 * dy - 2 * dx;
+	int dx2 = 2 * dx;
+	int dxdy2 = 2 * dx - 2 * dy;
+	int x = x1;
+	int y = y1;
+	int p;
+	if (dx>dy) {
+		p = 2 * dy - dx;
+		glBegin(GL_POINTS);
+		glVertex2i(x, y);
+		glEnd();
+		for (int i = 0; i < dx; i++) {
+			if (p < 0) {
+				p = p+dy2;
+				x = x + xinc;
+				glBegin(GL_POINTS);
+				glVertex2i(x, y);
+				glEnd();
+			}
+			else {
+				p = p + dydx2;
+				x = x + xinc;
+				y = y + yinc;
+				glBegin(GL_POINTS);
+				glVertex2i(x, y);
+				glEnd();
+			}
+		}
+    }
+	else {
+		p = 2 * dx - dy;
+		glBegin(GL_POINTS);
+		glVertex2i(x, y);
+		for (int i = 0; i < dy; i++) {
+			if (p < 0) {
+				p = p + dx2;
+				y = y + yinc;
+				glBegin(GL_POINTS);
+				glVertex2i(x, y);
+				glEnd();
+			}
+			else {
+				p = p + dxdy2;
+				x = x + xinc;
+				y = y + yinc;
+				glBegin(GL_POINTS);
+				glVertex2i(x, y);
+				glEnd();
+			}
+		}
+		glFlush();
+	}
+}
+
+void bresenhamcircle(int xc, int yc, int r) {
+	int p = 3 - 2*r;
+	int x = 0, y = r;
+	glBegin(GL_POINTS);
+	glVertex2i(x + xc, y + yc);
+	glVertex2i(x + xc, -y + yc);
+	glVertex2i(y + xc, x + yc);
+	glVertex2i(-y + xc, x + yc);
+	glEnd();
+	while (y > x) {
+		x = x + 1;
+		if (p <= 0) {
+			p = p + 4 * x + 6;
+		}
+		else {
+			p = p + 4 * x - 4 * y + 10;
+			y--;
+		}
+		if (y < x)
+			break;
+        glBegin(GL_POINTS);
+		glVertex2i(x + xc, y + yc);
+		glVertex2i(-x + xc, y + yc);
+		glVertex2i(x + xc, -y + yc);
+		glVertex2i(-x + xc, -y + yc);
+		glVertex2i(y + xc, x + yc);
+		glVertex2i(-y + xc, x + yc);
+		glVertex2i(y + xc, -x + yc);
+		glVertex2i(-y + xc, -x + yc);
+		glEnd();
+		glFlush();
+		usleep(100000);
+	}
+}
+
+float twoPlaces(float a){
+    return round(a * 100.0) / 100.0;
+}
+
+void floodFill(int x, int y, float* fillColor, float* interiorColor) {
+    float currentColor[3];
+    glReadPixels(x, y, 1.0, 1.0, GL_RGB, GL_FLOAT, currentColor);
+
+    std::stack<std::pair<int, int>> pixels;
+    pixels.push(std::make_pair(x, y));
+    while (!pixels.empty()) {
+        std::pair<int, int> currentPixel = pixels.top();
+        pixels.pop();
+        int cx = currentPixel.first;
+        int cy = currentPixel.second;
+        glReadPixels(cx, cy, 1.0, 1.0, GL_RGB, GL_FLOAT, currentColor);
+        //std::cout<<twoPlaces(currentColor[0])<<" "<<interiorColor[0]<<std::endl;
+        if (twoPlaces(currentColor[0]) == interiorColor[0] && twoPlaces(currentColor[1]) == interiorColor[1] && twoPlaces(currentColor[2]) == interiorColor[2]) {
+            glBegin(GL_POINTS);
+            glColor3fv(fillColor);
+            glVertex2i(cx, cy);
+            glEnd();
+            glFlush();
+            pixels.push(std::make_pair(cx + 1, cy));
+            pixels.push(std::make_pair(cx - 1, cy));
+            pixels.push(std::make_pair(cx, cy + 1));
+            pixels.push(std::make_pair(cx, cy - 1));
+        }
+    }
+}
+
+
+bool isBlinking = true;
+int blinkDuration = 500; // Blink duration in milliseconds
+int elapsedTime = 0; // Elapsed time in milliseconds
+int pending = 1;
+int arrowUpdatePending = 1;
+int arrowLevel=0;
+void drawArrow(){
+    if(isBlinking){
+        glColor3f(0.5,0.5,0.5);
+    }else{
+        glColor3f(0.56, 0.93, 0.56);
+    }
+        for(int i=0; i<=arrowLevel; i++){
+                 int x = i*50;
+            if(i>9 && i<13) continue;
+            bresenhamWithouDelay(175+x,255,195+x,255);
+            bresenhamWithouDelay(195+x,255,195+x,257);
+            bresenhamWithouDelay(195+x,257,207+x,250);
+            bresenhamWithouDelay(207+x,250,195+x,243);
+            bresenhamWithouDelay(195+x,243,195+x,245);
+            bresenhamWithouDelay(195+x,245,175+x,245);
+            bresenhamWithouDelay(175+x,245,175+x,255);
+        }
+        if(arrowLevel==9) arrowLevel+=3;
+}
+
+void drawVerticalArrow(){
+    if(isBlinking){
+        glColor3f(0.5,0.5,0.5);
+    }else{
+        glColor3f(0.56, 0.93, 0.56);
+    }
+        for(int i=0; i<2; i++){
+                 int x = i*20;
+//            bresenhamWithouDelay(480+x,185,480+x,205);
+//            bresenhamWithouDelay(480+x,205,478+x,205);
+            bresenhamWithouDelay(478+x,205,485+x,217);
+            bresenhamWithouDelay(485+x,217,492+x,205);
+            bresenhamWithouDelay(485+x,217,485+x,185);
+//            bresenhamWithouDelay(492+x,205,490+x,205);
+//            bresenhamWithouDelay(490+x,205,490+x,185);
+//            bresenhamWithouDelay(490+x,185,480+x,185);
+        }
+}
+
+
+void updateElapsedTime(int value)
+{
+    //std::cout<<elapsedTime<<std::endl;
+    // Update elapsed time
+    elapsedTime += blinkDuration;
+    if(elapsedTime>=20000)
+        arrowUpdatePending = 0;
+    if (elapsedTime >= 28000) {
+        pending = 0;
+    } else {
+        // Set timer for next elapsed time update
+        glutTimerFunc(blinkDuration, updateElapsedTime, 1);
+    }
+}
+
+
+void timer(int value)
+{
+    if(!pending) {
+        return;
+    }
+    // Toggle blinking on and off
+    isBlinking = !isBlinking;
+
+    // Set timer for next blink
+    glutTimerFunc(blinkDuration, timer, 0);
+
+    drawArrow();
+    if(arrowLevel>6) drawVerticalArrow();
+}
+void updateArrowLevel(int value)
+{
+    if(!arrowUpdatePending) {
+            glPointSize(10.0);
+            glColor3f(0.0,1.0,0.0);
+            glBegin(GL_POINTS);
+            glVertex2f(920.0,250.0);
+            glVertex2f(920.0,230.0);
+            glVertex2f(920.0,270.0);
+            glVertex2f(920.0,210.0);
+            glVertex2f(920.0,290.0);
+            glVertex2f(918.0,190.0);
+            glVertex2f(918.0,310.0);
+            glVertex2f(918.0,170.0);
+            glVertex2f(918.0,330.0);
+            glVertex2f(916.0,150.0);
+            glVertex2f(916.0,350.0);
+            glVertex2f(916.0,130.0);
+            glVertex2f(916.0,370.0);
+            glVertex2f(912.0,110.0);
+            glVertex2f(912.0,390.0);
+            glVertex2f(910.0,90.0);
+            glVertex2f(910.0,410.0);
+            glEnd();
+            glFlush();
+            glPointSize(2.0);
+            return;
+    }
+
+     arrowLevel++;
+    // Set timer for next blink
+    glutTimerFunc(2000, updateArrowLevel, 2);
+}
+
+
+void display() {
+  // your drawing code goes here
+  glClear(GL_COLOR_BUFFER_BIT);
+	glPointSize(2.0);
+	glLineWidth(3.0f);
+	glColor3f(0.0, 0.0, 0.0);
+
+	//front ellipse
+	drawEllipse(25, 180, 900, 250);
+
+
+	//slanting lines
+    bresenham(725,325,900,430);
+    bresenham(725,175,900,70);
+
+    //box
+    bresenham(725,325,725,175);
+    bresenham(725,175,50,175);
+    bresenham(50,175,50,325);
+    bresenham(50,325,725,325);
+
+
+    //slim box
+    bresenham(50,175,40,175);
+    bresenham(40,175,40,325);
+    bresenham(40,325,50,325);
+
+    //pins
+    bresenham(30,305,40,305);
+    bresenham(30,295,40,295);
+    bresenham(30,205,40,205);
+    bresenham(30,195,40,195);
+
+    //heater
+    bresenham(55,260,95,260);
+    bresenham(95,260,95,240);
+    bresenham(95,240,55,240);
+    bresenham(55,240,55,260);
+
+    //balls
+    bresenhamcircle(105,250,10);
+    bresenhamcircle(125,250,10);
+    bresenhamcircle(145,250,10);
+
+    //ballcover
+    bresenham(95,270,160,270);
+    bresenham(160,270,160,230);
+    bresenham(160,230,95,230);
+
+    //grid
+    bresenham(160,280,210,280);
+    bresenham(210,280,210,260);
+    bresenham(210,240,210,220);
+    bresenham(210,220,160,220);
+
+    // pre-accelerating node
+    bresenham(220,270,220,280);
+    bresenham(220,280,255,280);
+    bresenham(255,280,255,270);
+    bresenham(255,280,290,280);
+    bresenham(290,280,290,270);
+
+    bresenham(220,230,220,220);
+    bresenham(220,220,255,220);
+    bresenham(255,220,255,230);
+    bresenham(255,220,290,220);
+    bresenham(290,220,290,230);
+
+    // focusing node
+    bresenham(300,270,300,280);
+    bresenham(300,280,335,280);
+    bresenham(335,280,335,270);
+    bresenham(335,280,370,280);
+    bresenham(370,280,370,270);
+
+    bresenham(300,230,300,220);
+    bresenham(300,220,335,220);
+    bresenham(335,220,335,230);
+    bresenham(335,220,370,220);
+    bresenham(370,220,370,230);
+
+    // accelerating node
+    bresenham(380,270,380,280);
+    bresenham(380,280,415,280);
+    bresenham(415,280,415,270);
+    bresenham(415,280,450,280);
+    bresenham(450,280,450,270);
+
+    bresenham(380,230,380,220);
+    bresenham(380,220,415,220);
+    bresenham(415,220,415,230);
+    bresenham(415,220,450,220);
+    bresenham(450,220,450,230);
+
+    //vertical deflection plates
+    bresenham(470,265,470,275);
+    bresenham(470,275,550,275);
+    bresenham(550,275,550,265);
+    bresenham(550,265,470,265);
+
+    bresenham(540,262,535,272);
+    bresenham(535,272,595,310);
+    bresenham(595,310,600,300);
+    bresenham(600,300,540,262);
+
+    bresenham(470,235,470,225);
+    bresenham(470,225,550,225);
+    bresenham(550,225,550,235);
+    bresenham(550,235,470,235);
+
+    bresenham(540,238,535,228);
+    bresenham(535,228,595,190);
+    bresenham(595,190,600,200);
+    bresenham(600,200,540,238);
+
+
+    //Horizontal deflection plates
+    bresenham(670,300,760,300);
+    bresenham(760,300,760,250);
+    bresenham(760,250,670,250);
+    bresenham(670,250,670,300);
+
+    bresenham(670,252,770,252);
+    bresenham(770,252,770,202);
+    bresenham(770,202,670,202);
+    bresenham(670,202,670,252);
+
+    bresenham(761,302,758,252);
+    bresenham(758,252,820,265);
+    bresenham(820,265,820,315);
+    bresenham(820,315,761,302);
+
+    bresenham(771,250,768,200);
+    bresenham(768,200,830,187);
+    bresenham(830,187,830,237);
+    bresenham(830,237,771,250);
+
+
+    glPointSize(1.0);
+    //color it
+
+    //color strip
+    float newColor[3] = {0.0, 1.0, 1.0}; // cyan
+    float oldColor[3] = {br,bg,bb}; // light green
+    floodFill(41,300,newColor,oldColor);
+
+
+    //color heater
+    newColor[0] = 0.88;
+    newColor[1] = 0.34;
+    newColor[2] = 0.13;
+    oldColor[0] = br;
+    oldColor[1] = bg;
+    oldColor[2] = bb;
+    floodFill(60,250,newColor,oldColor);
+    //color electrons
+    newColor[0] = 0.0;
+    newColor[1] = 1.0;
+    newColor[2] = 0.0;
+    oldColor[0] = br;
+    oldColor[1] = bg;
+    oldColor[2] = bb;
+    floodFill(105,250,newColor,oldColor);
+    floodFill(125,250,newColor,oldColor);
+    floodFill(145,250,newColor,oldColor);
+
+    //vertical defection plates
+    newColor[0] = 0.0;
+    newColor[1] = 1.0;
+    newColor[2] = 1.0;
+    oldColor[0] = br;
+    oldColor[1] = bg;
+    oldColor[2] = bb;
+    floodFill(471,266,newColor,oldColor);
+    floodFill(545,270,newColor,oldColor);
+    floodFill(590,295,newColor,oldColor);
+    floodFill(475,230,newColor,oldColor);
+    floodFill(545,230,newColor,oldColor);
+    floodFill(590,195,newColor,oldColor);
+
+    //horizontal deflection plates
+    newColor[0] = 1.0;
+    newColor[1] = 0.65;
+    newColor[2] = 0.0;
+    oldColor[0] = br;
+    oldColor[1] = bg;
+    oldColor[2] = bb;
+    floodFill(680,290,newColor,oldColor);
+    floodFill(750,290,newColor,oldColor);
+    floodFill(680,240,newColor,oldColor);
+    floodFill(760,240,newColor,oldColor);
+    floodFill(780,270,newColor,oldColor);
+    floodFill(790,230,newColor,oldColor);
+    glPointSize(2.0);
+         glutTimerFunc(blinkDuration, timer, 0);
+    glutTimerFunc(blinkDuration, updateElapsedTime, 1);
+    glutTimerFunc(2000,updateArrowLevel,2);
+	glFlush();
+}
+
+
+int main(int argc, char** argv) {
+  glutInit(&argc, argv);
+  glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+  glutInitWindowSize(1100, 500);
+  glutInitWindowPosition(20, 100);
+  glutCreateWindow("CRTAnimation");
+  glClearColor(br,bg,bb, 0.5);
+  glMatrixMode(GL_PROJECTION);
+  gluOrtho2D(0, 1100, 0, 500);
+  glutDisplayFunc(display);
+  glutMainLoop();
+  return 0;
+}
